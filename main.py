37cb90db3c3115f757bf92ae190ea5543047b068
milenarmp <milenarmp@gmail.com>
2020-04-25 19:18:04 -0300

    Calculadora somando e subtraindo.

--- main.py
+++ main.py
@@ -12,10 +12,20 @@ class TelaInicial(Widget):
     somahora = ObjectProperty(None)
     minutosoma = ObjectProperty(None)
 
-    def btn(self):
+    def btnSomar(self):
         if len(self.hora.text) > 0 and len(self.minuto.text) > 0 and len(self.somahora.text) > 0 and len(self.minutosoma.text) > 0:
             inicio = datetime.datetime(2020, 1, 1, int(self.hora.text), int(self.minuto.text), 00, 000000)
             fim = inicio + datetime.timedelta(hours = int(self.somahora.text), minutes= int(self.minutosoma.text))
+            horastring = str(fim)
+            horaseparado = horastring.split()
+            self.horariosaida = horaseparado[1][:5]
+        else:
+            self.horariosaida = 'Formato inválido!'
+
+    def btnSubtrair(self):
+        if len(self.hora.text) > 0 and len(self.minuto.text) > 0 and len(self.somahora.text) > 0 and len(self.minutosoma.text) > 0:
+            inicio = datetime.datetime(2020, 1, 1, int(self.hora.text), int(self.minuto.text), 00, 000000)
+            fim = inicio - datetime.timedelta(hours = int(self.somahora.text), minutes= int(self.minutosoma.text))
             horastring = str(fim)
             horaseparado = horastring.split()
             self.horariosaida = horaseparado[1][:5]
