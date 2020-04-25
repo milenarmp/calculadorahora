bf5cd60a1103ebb1372b4797f5567fdae8a5f966
milenarmp <milenarmp@gmail.com>
2020-04-25 18:05:52 -0300

    Minimamente funcional.

---
+++ README.md
@@ -0,0 +1,4 @@
+# calculadorahora
+Calculadora de hora desenvolvida em Kivy 1.11.1 e Python
+
+A calculadora apenas está somando 09:48h devido ao turno de tempo do meu interesse.
---
+++ main.py
@@ -0,0 +1,28 @@
+import datetime
+import kivy
+kivy.require('1.11.1')
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty, StringProperty
+
+class TelaInicial(Widget):
+    hora = ObjectProperty(None)
+    minuto = ObjectProperty(None)
+    horariosaida = StringProperty()
+
+    def btn(self):
+        if len(self.hora.text) > 0 and len(self.minuto.text) > 0:
+            inicio = datetime.datetime(2020, 1, 1, int(self.hora.text), int(self.minuto.text), 00, 000000)
+            fim = inicio + datetime.timedelta(hours = 9, minutes= 48)
+            horastring = str(fim)
+            horaseparado = horastring.split()
+            self.horariosaida = horaseparado[1][:5]
+        else:
+            self.horariosaida = 'Formato inválido!'
+
+class calculadorahora(App): # <- Main Class
+    def build(self):
+        return TelaInicial()
+
+if __name__ == '__main__':
+    calculadorahora().run()
